2fc1793c37ebb5cd3616218e0d6be27fe8b5c913
NickRob229 <553594@chrome.escg.ac.uk>
2024-10-26 12:56:32 +0100

    Update before satisfactory

---
+++ Assets/MovingEnemy.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class MovingPlatforms : MonoBehaviour
+{
+    public Transform platform; // reference for the platform
+    public Transform startPoint; // reference for the start point
+    public Transform endPoint; // reference for the end point
+    public float speed = 1.5f; // speed between points
+    public int direction = 1; // direction of travel 1 or -1
+
+    // Function to show the lines between points
+    private void OnDrawGizmos()
+    {
+        // just for
+        if (platform != null && startPoint != null && endPoint != null)
+        {
+            Gizmos.DrawLine(platform.transform.position, startPoint.position);
+            Gizmos.DrawLine(platform.transform.position, endPoint.position);
+        }
+    }
+    // Additional Code 
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 target = currentMovementTarget();
+        platform.position = Vector2.Lerp(platform.position, target, speed * Time.deltaTime);
+        float distance = (target - (Vector2)platform.position).magnitude;
+        if (distance <= 0.1f)
+        {
+            direction *= -1;
+        }
+    }
+
+    // function to set the direction of travel
+    Vector2 currentMovementTarget()
+    {
+        if (direction == 1)
+        {
+            return startPoint.position;
+        }
+        else
+        {
+            return endPoint.position;
+        }
+    }
+    // End of Additional Code
+}
